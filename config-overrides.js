98033aa8e86e18752a8b861b815232ca77e98439
黄斌 <bin.huang02@hand-china.com>
2018-12-08 18:54:42 +0800

    module include less and suport less

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,20 @@
-const rewireLess = require('react-app-rewire-less');
- 
-/* config-overrides.js */
-module.exports = function override(config, env) {
-  config = rewireLess(config, env);
-  // with loaderOptions
-  // config = rewireLess.withLoaderOptions(someLoaderOptions)(config, env);
-  return config;
+// const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLessWithModule = require('react-app-rewire-less-with-modules')
+
+module.exports = function override (config, env) {
+  // config = injectBabelPlugin(['import', {
+  //   libraryName: 'antd',
+  //   libraryDirectory: 'es',
+  //   style: true
+  // }], config)
+  config = rewireLessWithModule(config, env, {
+    // modifyVars: {
+    //   '@primary-color': 'red',
+    //   '@link-color': '#1DA57A',
+    //   '@border-radius-base': '2px',
+    //   '@font-size-base': '16px',
+    //   '@line-height-base': '1.2'
+    // },
+  })
+  return config
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styles from './App.less';
+import styles from './App.module.less';
 
 class App extends React.Component {
   public render() {
