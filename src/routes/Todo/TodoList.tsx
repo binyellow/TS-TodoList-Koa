32782dc24baa0d862812a26bb712540c79faf508
黄斌 <bin.huang02@hand-china.com>
2018-12-15 13:13:17 +0800

    dispose static function problem in TS

--- src/routes/Todo/TodoList.tsx
+++ src/routes/Todo/TodoList.tsx
@@ -2,21 +2,68 @@ import React, { Component } from 'react';
 import { Table } from 'antd';
 import { connect } from 'react-redux';
 import { Bind } from 'lodash-decorators';
+import { getResponse, createPagination } from 'utils/utils';
+import { renderTimeStamp } from 'utils/render';
+import { fetchTodoList } from 'service/todo';
 import * as actions from '../../actions/todo';
 
 interface TodoListProps {
   todoState: any,
-  deleteTodo: any
+  deleteTodo: any,
+  addTodo: any,
+  userId: number,
 }
-class TodoList extends Component<TodoListProps> {
+interface S {
+  loading: boolean;
+}
+class TodoList extends Component<TodoListProps, S> {
+  // public static getDerivedStateFromProps(props: TodoListProps, state: S): any {
+  //   const { loading } = state;
+  //   const { loading: nextLoading } = props;
+  //   if( !loading && nextLoading !== loading ) {
+  //     return {
+  //       loading: nextLoading,
+  //     }
+  //   }
+  //   return null;
+  // }
+  constructor(props: TodoListProps) {
+    super(props);
+    this.state = {
+      loading: true,
+    }
+  }
+  public componentDidMount() {
+    const { addTodo, userId } = this.props;
+    fetchTodoList({ userId }).then(res=>{
+      const result = getResponse(res);
+      if(result) {
+        this.setState({ loading: false });
+        addTodo({ todoList: result.content, pagination: createPagination(result) });
+      }
+    })
+  }
   @Bind()
   public handleDelete(index: number) {
     const { deleteTodo } = this.props;
     deleteTodo({ index });
   }
-
+  @Bind()
+  public handleChangePage(pagination: { current: number, pageSize: number }) {
+    const { addTodo, userId } = this.props;
+    const { current, pageSize } = pagination;
+    this.setState({ loading: true });
+    fetchTodoList({ userId, current, pageSize }).then(res=>{
+      const result = getResponse(res);
+      if(result) {
+        addTodo({ todoList: result.content, pagination: createPagination(result) });
+        this.setState({ loading: false });
+      }
+    })
+  }
   public render() {
-    const { todoState: { todoList = [] } } = this.props;
+    const { loading = false } = this.state;
+    const { todoState: { todoList = [], pagination = {} } } = this.props;
     const columns = [
       {
         title: '用户id',
@@ -31,20 +78,25 @@ class TodoList extends Component<TodoListProps> {
       {
         title: '是否已完成',
         dataIndex: 'completed',
-        width: 150,
+        width: 100,
         render: (value: boolean) => value ? '是' : '否',
       },
       {
         title: '备忘时间',
         dataIndex: 'time',
-        width:150
+        width: 250,
+        render: renderTimeStamp,
       }
     ];
     const tableProps = {
       columns,
+      loading,
+      pagination,
       rowKey: '_id',
+      scroll: { y: 400 },
       bordered: true,
-      dataSource: todoList
+      dataSource: todoList,
+      onChange: this.handleChangePage,
     }
     return (
       <Table {...tableProps}/>
@@ -53,5 +105,5 @@ class TodoList extends Component<TodoListProps> {
 }
 export default connect(
   (state: any)=>({todoState: state.todo}),
-  { deleteTodo: actions.deleteTodo }
+  { deleteTodo: actions.deleteTodo, addTodo: actions.addTodo }
 )(TodoList);
